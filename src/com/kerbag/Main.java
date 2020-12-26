1ca7831bdba8376645f18306239faba7b4f5e440
Talal Kerbag <kerbagtalal94@gmail.com>
2020-12-26 11:00:21 -0500

    Fixed an issue where if there are no items in a StockList object, adjusting a certain stock would have returned null

--- src/com/kerbag/Main.java
+++ src/com/kerbag/Main.java
@@ -93,8 +93,13 @@ public class Main {
 
 //        temp = new StockItem("pen",1.12);
 //        stockList.Items().put(temp.getName(),temp);
-        stockList.Items().get("car").adjustStock(2000);
-        stockList.get("car").adjustStock(-1000);
+        StockItem car = stockList.Items().get("car");
+        if(car != null){
+            car.adjustStock(2000);
+        }
+        if (car != null) {
+            stockList.get("car").adjustStock(-1000);
+        }
 //        System.out.println(stockList);
 //        for(Map.Entry<String,Double> prices: stockList.PriceList().entrySet()){
 //            System.out.println(prices.getKey() + " costs " + prices.getValue());
